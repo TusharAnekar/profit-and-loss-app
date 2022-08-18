dabb32198c8da15dc823f71d470e3487f88a3bb3
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-08-18 20:08:55 +0530

    profit loss percentage logic fix

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ function calculateProfitAndLoss (initial, quantity, current) {
     else {
         if (initial < current) {
             var profit = (current - initial) * quantity;
-            var profitPercentage = (profit / initial) * 100;
+            var profitPercentage = (profit / (initial * quantity)) * 100;
             result.style.color = "green";
             result.innerText = `Your profit is ${profit.toFixed(2)} and profit percentage is ${profitPercentage.toFixed(2)}%`;
         }
         else if (initial > current) {
             var loss = (initial - current) * quantity;
-            var lossPercentage = (loss / initial) * 100;
+            var lossPercentage = (loss / (initial * quantity)) * 100;
             result.style.color = "red";
             result.innerText = `Your loss is ${loss.toFixed(2)} and loss percentage is ${lossPercentage.toFixed(2)}%`;
         }
